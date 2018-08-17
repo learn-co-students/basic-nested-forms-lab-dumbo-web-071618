ebdf1e10699e23ddc0f5ef655c7bcbccfba5de24
joalorro <joalorro@gmail.com>
2018-08-17 17:28:18 -0400

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,15 @@
 class Recipe < ActiveRecord::Base
-  has_many :ingredients 
+  has_many :ingredients
+  # accepts_nested_attributes_for :ingredients
+
+  def ingredients_attributes= ings
+    # byebug
+    ings.each do |num,ing_hash|
+      # ing_to_be_added = Ingredient.find_or_create_by ing_hash
+      # ingredients << ing_to_be_added
+      # save
+      self.ingredients.build ing_hash
+
+    end
+  end
 end
